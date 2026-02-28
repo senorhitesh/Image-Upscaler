da17b8a75493e321d647bb6e7b2391c1fe80c16f
Hitesh Suthar <senorhitesh@gmail.com>
2026-02-28 22:46:18 +0530

    feat(ui):- basic ui created

--- Image-Upscaler/src/App.jsx
+++ Image-Upscaler/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Home from './components/Home'
+import Footer from './components/Footer'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <>
+    <div className='flex h-screen items-center justify-center flex-col bg-gray-50 '>
+      <h1 className='text-3xl font-bold text-gray-950'>AI Image Enhancer</h1>
+      <p className='text-gray-700'>Upload your Image and let AI enhance in seconds !</p>
+    </div>
+    <Home/>
+  <Footer/>
+  </>
 }
 
 export default App
---
+++ Image-Upscaler/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageUpload from './ImageUpload'
+import ImagePreview from './ImagePreview'
+
+const Home = () => {
+  return (
+    <>
+    <ImageUpload/>
+    <ImagePreview/>
+    </>
+  )
+}
+
+export default Home
