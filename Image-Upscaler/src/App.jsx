c7a8ff176505730082db509473ce58b227cc62ab
Hitesh Suthar <senorhitesh@gmail.com>
2026-03-01 21:21:15 +0530

    app layout created

--- Image-Upscaler/src/App.jsx
+++ Image-Upscaler/src/App.jsx
@@ -4,12 +4,11 @@ import Footer from './components/Footer'
 
 const App = () => {
   return <>
-    <div className='flex h-screen items-center justify-center flex-col bg-gray-50 '>
+    <div className='flex h-screen items-center justify-center flex-col  bg-gray-50 '>
       <h1 className='text-3xl font-bold text-gray-950'>AI Image Enhancer</h1>
       <p className='text-gray-700'>Upload your Image and let AI enhance in seconds !</p>
+          <Home/>
     </div>
-    <Home/>
-  <Footer/>
   </>
 }
 
--- Image-Upscaler/src/components/ImagePreview.jsx
+++ Image-Upscaler/src/components/ImagePreview.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const ImagePreview = () => {
   return (
-    <div>ImagePreview</div>
+    <div className='mt-8 grid-cols-1 grid md:grid-cols-2 gap-6 w-full max-w-4xl  '>
+      <div  className='bg-white h-full ' >
+        <h2 className='bg-neutral-800 text-center rounded-t-2xl w-full text-white font-medium px-4 py-1 flex flex-col'>Original Image</h2>
+        <img src="" alt="" className='object-fit w-full h-full ' />
+      </div>
+
+      <div className='bg-white h-full ' >
+        <h2 className='bg-purple-800 text-center rounded-t-2xl w-full text-white font-medium px-4 py-1 flex flex-col'>Enhanced Image</h2>
+        <img src="" alt="" className='object-fit w-full h-full ' />
+      </div>
+    </div>
   )
 }
 
