740c17ace89c1767450e72c0fcae38df7f093e99
Hitesh Suthar <senorhitesh@gmail.com>
2026-03-01 22:36:29 +0530

    loader added

--- Image-Upscaler/src/components/ImagePreview.jsx
+++ Image-Upscaler/src/components/ImagePreview.jsx
@@ -10,9 +10,9 @@ const ImagePreview = (props) => {
           {props.uploaded ? (<img src={props.uploaded} alt="Image" className='object-cover w-full h-full ' />) : (<div className='text-center mt-1.5 text-gray-500'> <p>Upload Image to Preview</p> </div>) }
       </div>
 
-      <div className='bg-white h-[400px] shadow-xl   ' >
+      <div className='bg-white h-[400px] shadow-xl  flex flex-col items-center' >
         <h2 className='bg-purple-800 text-center rounded-t-2xl w-full text-white font-medium px-4 py-1 flex flex-col'>Enhanced Image</h2>
-        {props.enhanced && props.loading && (<img src={props.uploaded} alt="Image" className='object-fit w-full h-full ' />)  }
+        {props.enhanced && props.loading &&  (<img src={props.uploaded} alt="Image" className='object-fit w-full h-full ' />)  }
           
           {props.loading ? <Loading/>:  <div className='text-center mt-1.5 text-gray-500'> <p>Upload Image to Enhance</p> </div> }
       </div>
--- Image-Upscaler/src/components/Loading.jsx
+++ Image-Upscaler/src/components/Loading.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { TailSpin } from 'react-loader-spinner'
+
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+   <TailSpin
+  height="80"
+  width="80"
+  radius="9"
+  color="#4fa94d"
+  ariaLabel="three-dots-loading"
+  wrapperStyle={{ margin: '20px' }}
+  wrapperClass="custom-loader"
+  visible={true}
+/>
   )
 }
 
