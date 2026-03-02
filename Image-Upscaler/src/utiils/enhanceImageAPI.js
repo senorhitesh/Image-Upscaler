4e0caf9ea8159bb1c776d00661027249562a6e4d
Hitesh Suthar <senorhitesh@gmail.com>
2026-03-02 22:08:04 +0530

    Poll created fot Enhanced Emage

--- Image-Upscaler/src/utiils/enhanceImageAPI.js
+++ Image-Upscaler/src/utiils/enhanceImageAPI.js
@@ -42,3 +42,19 @@ const FetchEnhancedImage = async (taskId) => {
   );
   return data.data.image;
 };
+
+const PollForEnhnacedImage = async (taskID, retries = 0) => {
+  const result = await FetchEnhancedImage(taskID);
+
+  if (result.status === 4) {
+    console.log(`Processing......`);
+
+    if (retries >= 10) {
+      throw new Error(`Max Limit Reach Please try agian Later`);
+    }
+
+    await Promise((resolve) => setTimeout(resolve, 2000));
+
+    return PollForEnhanacedImage(taskID, retries + 1);
+  }
+};
